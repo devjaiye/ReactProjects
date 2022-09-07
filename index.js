f4a3c92a7ce2324a16789af6fc5d161df7c01d90
devjaiye <dayojaiye74@gmail.com>
2022-09-07 23:01:19 +0100

    imperative code

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-function Main(){
-    return(
-        <h1>This is the Main file</h1>
-    )
-}
+// function Main(){
+//     return(
+//         <h1>This is the Main file</h1>
+//     )
+// }
 
-ReactDOM.render(
-    <div> 
-        <Main/>
-    </div>, 
-    document.getElementById('root')
-)
+// ReactDOM.render(
+//     <div> 
+//         <Main/>
+//     </div>, 
+//     document.getElementById('root')
+// )
+
+
+//... The JS code belove is an imperative way to program, React allows declarative  
+const h1 = document.createElement('h1')
+h1.textContent = "Imperative programming..."
+h1.className = "header"
+
+document.getElementById('root').append(h1)
